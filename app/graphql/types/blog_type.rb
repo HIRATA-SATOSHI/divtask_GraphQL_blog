9536d3c96551b25fcfff93500017632ca1643bb6
satoshi.hirata <s.hirata0507@gmail.com>
2020-09-09 20:55:11 +0900

    Syntax Error 解決

--- app/graphql/types/blog_type.rb
+++ app/graphql/types/blog_type.rb
@@ -2,5 +2,5 @@ Types::BlogType = GraphQL::ObjectType.define do
   name "Blog"
   field :id, !types.ID
   field :title, !types.String
-  field :text, !Types::StringType
+  field :text, !types.String
 end
